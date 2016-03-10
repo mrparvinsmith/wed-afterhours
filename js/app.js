0bd42efc2af674dea714df026dce4cd3548ab37d
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-03-09 19:46:42 -0800

    added basic html, css, two name arrays

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var group1 = ['Christina', 'Adam', 'Ahmed', 'Singh', 'Ian'];
+
+for(var i = 0; i<group1.length; i ++) {
+  console.log(group1[i]);
+}
+
+var group2 = ['Ilya', 'Jenny', 'David', 'George', 'Allie'];
+
+var index2 = 0;
+
+while (index2 < group2.length) {
+  console.log(group2[index2]);
+  index2 ++;
+}
